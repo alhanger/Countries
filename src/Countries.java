54c6da68c53bac3d69063dafbc62cacb8d4a1847
alhanger <hanger129@gmail.com>
2015-10-15 16:54:58 -0400

    Added notes

--- src/Countries.java
+++ src/Countries.java
@@ -13,20 +13,20 @@ public class Countries {
         String[] lines = countriesContent.split("\n"); //puts each line of text into an array
 
         for (String line : lines) {
-            String[] columns = line.split("\\|");
+            String[] columns = line.split("\\|"); //splits abbreviations and names and stores them in an array for each country
 
             String abrv = columns[0];
             String countryName = columns[1];
-            Country country = new Country(countryName, abrv);
-            String countryLetter = String.valueOf(country.name.charAt(0));
+            Country country = new Country(countryName, abrv); //creates new Country object for each country
+            String countryLetter = String.valueOf(country.name.charAt(0)); //stores first letter of a country
 
-            ArrayList<Country> list = listOfCountries.get(countryLetter);
-            if (list == null) {
+            ArrayList<Country> list = listOfCountries.get(countryLetter); //returns a list of all countries that start with countryLetter
+            if (list == null) { //create a list by the first letter of the country
                 list = new ArrayList<>();
                 list.add(country);
                 listOfCountries.put(countryLetter, list);
             }
-            else {
+            else { //adds the country to the list
                 list.add(country);
             }
         }
@@ -37,12 +37,12 @@ public class Countries {
         String entry = scanner.nextLine().toLowerCase();
         String output = "";
 
-        if (listOfCountries.containsKey(entry)) {
-            for (Country place : listOfCountries.get(entry)) {
-                output += (String.format("%s %s\n", place.abbrev, place.name));
+        if (listOfCountries.containsKey(entry)) { //searches for countries in the hashmap using the user input as the key
+            for (Country place : listOfCountries.get(entry)) { //loops over all countries that start with user input
+                output += (String.format("%s %s\n", place.abbrev, place.name)); //returns the search results and stores it in output
             }
         }
-        writeFile(String.format("%s_countries.txt", entry), output);
+        writeFile(String.format("%s_countries.txt", entry), output); //saves results to a file and names the file by the user input
     }
 
     static String readFile(String fileName) {
